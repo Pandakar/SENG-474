ef8413f9a4e9e8d8333b8d760ba3cc390e637e38
Myan Panikkar <myanpanikkar@gmail.com>
2017-02-13 17:01:22 -0800

    Perceptron-finished
    Completed perceptron implementation, could add further error checking.
    Output is learned values and a graph corresponding to the learned
    separator line.

--- assign2/q1/perceptron.py
+++ assign2/q1/perceptron.py
@@ -1,17 +1,17 @@
 '''
 Written for Python 3.5.2
 This program is an implementation of the Perceptron algorithm.
+Tested using a .csv file as input with only numeric attributes.
+Output is the values learned and a graph of the separator line.
 '''
 import numpy as np
 import os, sys
-from random import random
-# http://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
-# Read through later and see if this is helpful for reducing redundancy between assignment programs
-# Input: file name corresponding to file we wish to read
-# Output: contents of file if it can be read, otherwise an error code
-# This function handles errors related to invalid data input.
-# Current list of possible invalid errors:
-#   - inputted file has no way of being data (not .txt, .csv, .arff)
+import matplotlib.pyplot as pyplot
+################################################################################
+# read_file(fname)                                                             #
+# Input: file name corresponding to file we wish to read                       #
+# Output: contents of file if it can be read, otherwise an error code          #
+################################################################################
 def read_file(fname):
     if os.path.exists(fname):
         # First, check for proper extension
@@ -19,7 +19,7 @@ def read_file(fname):
         if fext not in ('txt', 'csv','arff'):
             print("Invalid input file."
                   "Looking for a file of .arff, .csv or .txt format")
-            exit(0)
+            exit(1)
         lines = open(fname, 'r')
         # split header row into attributes and classval
         header = lines.readline().strip('\n').split(',')
@@ -37,34 +37,44 @@ def read_file(fname):
         for line in lines:
             line = line.strip('\n').split(',')
             # Dummy data only filled if length not adequate
-            classvals.append(line[-1:])
+            classvals.append(int(line[-1]))
             line = line[:-1]
             while len(line) < 3:
                 line += [1]
-            traindata.append(line)
+            tmpline = []
+            for item in line:
+                try:
+                    tmpline.append(float(item))
+                except ValueError:
+                    tmpline.append(item)
+            traindata.append(tmpline)
         traindata = np.matrix(traindata)
-        classvals = np.matrix(classvals)
-        print(traindata)
-        print(classvals)
         return (traindata, classvals, header)
-    # If we hit this the file we're looking for is not in the current directory
-    print("Error: not found in cwd. Input valid data.", fname)
-    exit(0)
-
-# Input: data
-# Output: set of w's corresponding to separator line
+    # If we hit this the file we're looking for is not in its expected directory
+    print("Error: '{}' not found. Input valid data.".format(fname))
+    exit(1)
+################################################################################
+# Input: data                                                                  #
+# Output: set of w's corresponding to separator line                           #
+################################################################################
 def perceptron(traindata, classvals, dataheaders):
     # We want at least a 2D line, so ensure there are at least 3 w values
-    w = np.matrix([random() for _ in range(len(dataheaders)-1)])
+    w = np.matrix([np.random.rand(len(dataheaders)-1)])
     print("Randomly generated w values:", w)
-    nrows = w.shape[0]
-    i = 0
+    # eta can be set to any value such that 0 < eta <= 1
     eta = 0.01
-    correctlyClassified = False
-    #while not correctlyClassified :
-    #    if i
-    #h(x, w) = sign(w^Tx) = sign(w dot x)
-
+    # t is the number of training loops we conduct. Alter as desired
+    for t in range(100):
+        i = 0
+        for row in traindata:
+            sign = row.dot(w.transpose())
+            if classvals[i]*row.dot(w.transpose()) <= 0:
+                w = w + eta*classvals[i]*row
+            i += 1
+    return w.tolist()
+################################################################################
+#                     P R O G R A M    S T A R T S    H E R E                  #
+################################################################################
 def main():
     if len(sys.argv) != 2:
         print("Not enough arguments. Usage of program: perceptron.py inputfile")
@@ -72,7 +82,16 @@ def main():
     print("Attempting to read", sys.argv[1])
     # Get back a tuple containing data & headers in correct order
     traindata = read_file(sys.argv[1])
-    print(traindata[2][-1], "is the class value")
-    perceptron(traindata[0], traindata[1], traindata[2])
+    sepline = perceptron(traindata[0], traindata[1], traindata[2])[0]
+
+    print("Learned w values:", sepline)
+    print("Generating plot using w values to find points on line.")
+    graphline = []
+    # x1 = -(b+w2x2)/w1
+    for i in range(10):
+        graphline.append(-(sepline[0]*i+sepline[2])/sepline[1])
+    pyplot.plot(graphline)
+    pyplot.suptitle("Perceptron line for {}".format(sys.argv[1]))
+    pyplot.show()
 if __name__ == "__main__":
     main()
