8d61c9363421f62efc2ef77a68971c9529ed4297
Myan Panikkar <myanpanikkar@gmail.com>
2017-02-15 14:13:39 -0800

    q2-base-complete
    Can read input files and does... something with the inputted data. Not
    sure if it's doing it correctly.

--- assign2/q2/linear_regression.py
+++ assign2/q2/linear_regression.py
@@ -1,6 +1,5 @@
 '''
 Written for Python 3.5.2
-
 This program is an extension of the linear regression algorithm featured in this course.
 This extension adds the following capabiltiies:
 - Reading data from files
@@ -9,46 +8,74 @@ This extension adds the following capabiltiies:
 - Plotting error vector as a curve for output
 - Find a good learning rate based on error curve
 '''
-import numpy, matplotlib, pandas
-import os
-def linear_regression():
-    x = numpy.matrix([[1,1],[1,2]])
-    y = numpy.matrix([[1.5], [2]])
-    w = numpy.array([[2], [2]])
-    kappa = 0.1
-    n = len(y)
-    # Need to transform the given Octave equation into Python equivalent
-    # The '  symbol refers to transpose
-    # The .* symbol refers to elementwise multiplication
-    #     x*w produces a dot product?
-    for t in range(1,11):
-        # w = w + kappa*(1/n)*sum((y-x*w).*x)'
-        # -> w = w + kappa*(1/n)*
-        print(t)
+import numpy as np, matplotlib, pandas
+import os, sys
+import random
+################################################################################
+# linear_regression()
+################################################################################
+def linear_regression(traindata, classvals):
+    w = np.matrix([random.sample(range(-10,10), len(traindata.transpose()))])
+    print("Initial w vector:", w)
+    kappa = 0.9
+    n = len(traindata)
+    # Learn the proper weights for w after initializing
+    # Run for 100 loops or until w doesn't change, whichever comes first
+    for t in range(100):
+        i = 0
+        initw = w
+        for x in traindata:
+            w = w + kappa/n*(np.sum(np.multiply((classvals[i]-x.dot(w.transpose())), x).transpose()))
+            i += 1
+        if ((w == initw[0]) - 0).all():
+            break
+    # DEBUG - printing /99 since range(100) = [0, 99]
+    print("w vector settled to {} after {}/99 loops".format(w, t))
+    return w
+################################################################################
+# read_file(fname)
 # Input: file name corresponding to file we wish to read
 # Output: contents of file if it can be read, otherwise an error code
+################################################################################
 def read_file(fname):
-    # if fname exists in os:
-    #     try:
-    #         read(fname)
-    #         return file_contents
-    #     except file_errors:
-    #         return corresponding error code
-    pass
-
+    if os.path.exists(fname):
+        f = open(fname)
+        # Don't really care about labels for now
+        lines = f.readline()
+        lines = f.readlines()
+        traindata = np.matrix(list(map(lambda l: list(map(float, l.split(',')[:-1])), lines)))
+        # Repeat operation but to grab all classvals in an ordered list
+        classvals = list(map(lambda l: int(l.strip().split(',')[-1]), lines))
+        return (traindata, classvals)
+################################################################################
+# scale_attributes
+################################################################################
 def scale_attributes():
     pass
-
+################################################################################
+# compute_error
+################################################################################
 def compute_error():
     pass
-
+################################################################################
+# plot_error()
+################################################################################
 def plot_error():
     pass
-
+################################################################################
+# error_learning()
+################################################################################
 def error_learning():
     pass
+################################################################################
+#                   P R O G R A M    S T A R T S    H E R E                    #
+################################################################################
 def main():
-    print("Hello, world!")
-
+    if len(sys.argv) < 2:
+        print("Error: program incorrectly invoked."
+              "Usage of program: 'python linear_regression.py $inputfile'")
+        exit(0)
+    fdata = read_file(sys.argv[1])
+    linear_regression(fdata[0], fdata[1])
 if __name__ == "__main__":
     main()
