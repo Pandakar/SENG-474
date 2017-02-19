3767691482d844a72f15a476f4778213bda9dbfc
Myan Panikkar <myanpanikkar@gmail.com>
2017-02-19 12:55:27 -0800

    naive_bayes i/o
    Linear regression partially completed, naive bayes has the capability of
    reading in the training data and associated training labels.

--- assign2/q2/linear_regression.py
+++ assign2/q2/linear_regression.py
@@ -8,22 +8,26 @@ This extension adds the following capabiltiies:
 - Plotting error vector as a curve for output
 - Find a good learning rate based on error curve
 '''
-import numpy as np, matplotlib, pandas
+import numpy as np
+from matplotlib import pyplot
 import os, sys
 import random
 ################################################################################
 # linear_regression()
 ################################################################################
 def linear_regression(traindata, classvals):
-    w = np.matrix([random.sample(range(-10,10), len(traindata.transpose()))])
+    #w = np.matrix([random.sample(range(-10,10), len(traindata.transpose()))])
+    w = np.matrix([random.sample(range(-2,2), len(traindata.transpose()))])
     print("Initial w vector:", w)
-    kappa = 0.9
+    kappa = 1
+    error = []
     n = len(traindata)
     # Learn the proper weights for w after initializing
     # Run for 100 loops or until w doesn't change, whichever comes first
     for t in range(100):
         i = 0
         initw = w
+        error.append(compute_error(traindata, classvals, w))
         for x in traindata:
             w = w + kappa/n*(np.sum(np.multiply((classvals[i]-x.dot(w.transpose())), x).transpose()))
             i += 1
@@ -31,6 +35,7 @@ def linear_regression(traindata, classvals):
             break
     # DEBUG - printing /99 since range(100) = [0, 99]
     print("w vector settled to {} after {}/99 loops".format(w, t))
+    print("Error vector:", error)
     return w
 ################################################################################
 # read_file(fname)
@@ -50,18 +55,40 @@ def read_file(fname):
 ################################################################################
 # scale_attributes
 ################################################################################
-def scale_attributes():
-    pass
+def scale_attributes(traindata):
+    newmatrix = []
+    collen = len(traindata)
+    for column in traindata.T:
+        column = column.tolist()
+        newcol = []
+        valrange = np.amax(column) - np.amin(column)
+        avgval = np.mean(column)
+        print("Value Range: {} | Avg: {}".format(valrange, avgval))
+        for val in column[0]:
+            newcol.append( (val-avgval)/(valrange) )
+        newmatrix.append(newcol)
+    newmatrix.append([1]*collen)
+    traindata = (np.matrix(newmatrix)).T
+    print(traindata)
+    return traindata
 ################################################################################
 # compute_error
 ################################################################################
-def compute_error():
-    pass
+def compute_error(traindata, classvals, w):
+    n = len(traindata)
+    err,i = 0,0
+    for x in traindata:
+        err += ((classvals[i] - float(x.dot(w.T)))**2)
+        i += 1
+    err = err/(2*n)
+    print(err)
+    return err
 ################################################################################
 # plot_error()
 ################################################################################
-def plot_error():
-    pass
+def plot_error(error_vector):
+    pyplot.plot(error_vector)
+    pyplot.show()
 ################################################################################
 # error_learning()
 ################################################################################
@@ -76,6 +103,8 @@ def main():
               "Usage of program: 'python linear_regression.py $inputfile'")
         exit(0)
     fdata = read_file(sys.argv[1])
-    linear_regression(fdata[0], fdata[1])
+    scaledata = scale_attributes(fdata[0])
+    error = linear_regression(scaledata, fdata[1])
+    plot_error(error)
 if __name__ == "__main__":
     main()
--- assign2/q4/naive_bayes.py
+++ assign2/q4/naive_bayes.py
@@ -9,23 +9,57 @@ messages that can be split into two classes:
      - messages that just contain a wise saying
 
 The implementation of Naive Bayes here is a multinomial approach omitting
-feature selection. We consider all terms within our training data.  
+feature selection. We consider all terms within our training data.
 '''
 import numpy, matplotlib
-
-# Input: file name corresponding to file we wish to read
-# Output: contents of file if it can be read, otherwise an error code
-def read_file(fname):
-    # if fname exists in os:
-    #     try:
-    #         read(fname)
-    #         return file_contents
-    #     except file_errors:
-    #         return corresponding error code
-    pass
+import os, sys
+################################################################################
+# update_dictionary(d, words)                                                  #
+# INPUT:  a dictionary and the words we want to insert                         #
+# OUTPUT: updated dictionary                                                   #
+################################################################################
+def update_dictionary(d, words):
+    keys = d.keys()
+    for word in words:
+        if word in keys:
+            d[word] += 1
+        else:
+            d[word] = 1
+    return d
+################################################################################
+# read_data(datafile, labelfile)                                               #
+# Input: two file names, one for the data and one for the data's class vals    #
+# Output: a dictionary for each class with word frequencies                    #
+################################################################################
+def read_data(datafile, labelfile):
+    if os.path.exists(datafile) and os.path.exists(labelfile):
+        labels = (open(labelfile, 'r')).readlines()
+        data = (open(datafile, 'r')).readlines()
+        predictions = {} # for all labels == 1
+        sayings = {}     # for all labels == 0
+        for i in range(len(data)):
+            sentence = data[i].strip('\n').split()
+            if int(labels[i]) == 1:
+                predictions = update_dictionary(predictions, sentence)
+            else:
+                sayings = update_dictionary(sayings, sentence)
+        return (predictions, sayings)
+    else:
+        print("Error: one of {} or {} not at correct location.".format(datafile, labelfile))
+        exit(0)
 
 def main():
-    print("Hello, world!")
+    if len(sys.argv) != 5:
+        print("Error: need 4 additional arguments for program. Run program as follows:"
+              "python naive_bayes.py traindata trainlabels testdata testlabels")
+        exit(0)
+    # 1) Read the training data and the training labels
+    predictions, sayings = read_data(sys.argv[1], sys.argv[2])
+    # 3) Build a learning scheme based on training data & labels
+    # 4) Read the testing data
+    # 5) Read the testing labels
+    # 6) Run against the testing data, return a matrix pertaining to guesses
+    # 7) Verify accuracy against testing labels
 
 if __name__ == "__main__":
     main()
